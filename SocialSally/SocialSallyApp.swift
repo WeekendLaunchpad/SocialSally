a2fb7f307dd0d2105ee88fb3c94a11e5681cffb5
Livode Pvt. Ltd <hello@livode.com>
2021-09-16 11:57:00 +0530

    Initial Commit

---
+++ SocialSally/SocialSallyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialSallyApp.swift
+//  SocialSally
+//
+//  Created by Eddie Gear on 16/09/21.
+//
+
+import SwiftUI
+
+@main
+struct SocialSallyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
